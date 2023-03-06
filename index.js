439efa86494bc4f4515586304fd587069625b34e
Ogooluwakiitan <ogooluwaoladimeji@gmail.com>
2023-03-06 10:10:26 +0100

    array-function

---
+++ index.js
@@ -0,0 +1,85 @@
+// QUESTION 1
+// Mutation in context of JavaScript arrays is basically changing the array itself instead of returning a new array with the new changes whereas Non-Mutation is returning a whole new array which has all the changes.
+
+// Examples of Mutation array methods are
+// array.splice()
+// array.unshift()
+// array.sort()
+// array.push(),
+// array.fill()
+
+// Examples of non-mutation array methods are
+// array.concat(),
+// array.includes()
+// array.indexOf
+// array.join()
+// array.lastIndexOf
+// array.slice()
+
+// QUESTION 2
+let languages = ["C#", "JavaScript", "Ruby", "PHP", "Python"];
+
+// add 'kotlin' to the end of the array
+languages.push("Kotlin");
+console.log(languages);
+//[ 'C#', 'JavaScript', 'Ruby', 'PHP', 'Python', 'Kotlin' ]
+
+// add Java after Ruby
+languages.splice(3, 0, "Java");
+console.log(languages);
+// [ 'C#', 'JavaScript', 'Ruby', 'Java', 'PHP', 'Python', 'Kotlin' ]
+
+//Remove the first item in the array
+languages.shift("C#");
+console.log(languages);
+// [ 'JavaScript', 'Ruby', 'Java', 'PHP', 'Python', 'Kotlin' ]
+
+// Add ’Scala’ and ‘Swift’ to the beginning of the array
+languages.unshift("Scala", "Swift");
+console.log(languages);
+/*
+[
+  'Scala',      'Swift',
+  'JavaScript', 'Ruby',
+  'Java',       'PHP',
+  'Python',     'Kotlin'
+]
+ */
+
+// Replace ‘PHP’ with ‘Go’ and ‘Rust’
+languages.splice(5, 1, "Go", "Rust");
+console.log(languages);
+/*
+[
+  'Scala',      'Swift',
+  'JavaScript', 'Ruby',
+  'Java',       'Go',
+  'Rust',       'Python',
+  'Kotlin'
+]
+*/
+
+let fruit = ["apple", "mango", "banana"];
+function changeFruit(fruit) {
+    fruit[2] = "orange";
+    return fruit;
+}
+console.log(changeFruit(fruit)); // [ 'apple', 'mango', 'orange' ]
+
+// QUESTION 4
+
+
+
+// QUESTION 5
+function valTimesIndex(arrayOfNum) {
+    return arrayOfNum.map(function (value, index) {
+        return value * index;
+    });
+}
+
+let arrayOfNum = [5, 10, 15];
+let multipliedArray = valTimesIndex(arrayOfNum);
+
+console.log(multipliedArray);
+// [ 0, 10, 30 ]
+
